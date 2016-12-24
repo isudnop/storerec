2ba282fdff3fa648f37fa4f4ed2676dbe35512dc
Noppadol Mututanon <noppadol.mututanon@gmail.com>
2016-12-24 17:00:39 +0700

    Gulp , pagekage json , bootstrap sh*t  :boom:

--- gulpfile.js
+++ gulpfile.js
@@ -17,3 +17,10 @@ elixir(mix => {
     mix.sass('app.scss')
        .webpack('app.js');
 });
+
+elixir(function(mix) {
+  var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+  mix.sass('app.scss')
+    .copy(bootstrapPath + '/fonts', 'public/fonts')
+    .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js');
+});
