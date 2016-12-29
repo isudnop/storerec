e7b9a8b9b4c82bb10b259154d5da3a09dd95a9b9
Noppadol Mututanon <noppadol.mututanon@gmail.com>
2016-12-29 23:38:35 +0700

    Edit gulpfile :lipstick:

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,7 @@ elixir(function(mix) {
     var chartJsPath = 'node_modules/chart.js/dist';
     mix.copy(chartJsPath + '/Chart.min.js', 'public/js');
     
-    mix.copy('resources/assets/js/jquery-3.1.1.min.js', 'public/js');
-    mix.copy('resources/assets/js/jquery-ui.min.js', 'public/js');
+    mix.copy('resources/assets/js', 'public/js');
     mix.copy('resources/assets/sass/jquery-ui.css', 'public/css');
     mix.copy('resources/assets/images', 'public/css/images');
     
